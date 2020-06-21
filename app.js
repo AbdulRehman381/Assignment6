dcfd56ac66dadbd56c234fd6c4f05cad19db8668
abdul rehman <ranaabdulrehman902@gmail.com>
2020-06-21 16:37:42 +0500

    assignment6

---
+++ app.js
@@ -0,0 +1,26 @@
+//Chpter 21-25
+
+//Task # 1
+
+/*
+
+var firstname=prompt("Enter the first name");
+var lastname=prompt("Enter the last name ");
+var fullname=(firstname + " "+ lastname);
+alert (fullname);
+
+*/
+
+// Task # 2
+
+
+/*
+
+var a=prompt("Enter favourite phone name");
+var n=a.length;
+document.write("My favourite phone is:"+a );
+document.write("<br>");
+
+document.write("Length Of String:" +n);
+
+*/
